31232cf1d03510ad42a85caabea16b10e1f19c0d
Duc Anh <anh.chuduc@politetech.com>
2024-08-16 09:41:37 +0700

    Set up tailwindcss

--- web-app/src/views/HomeView/HomeView.tsx
+++ web-app/src/views/HomeView/HomeView.tsx
@@ -1,7 +1,12 @@
 import React from 'react';
 
 const HomeView: React.FC = () => {
-  return <div>Hello World</div>;
+  return (
+    <div className="font-bold">
+      Hello World
+      <div className="w-52 h-52 bg-[var(--color-primary)]"></div>
+    </div>
+  );
 };
 
 export default HomeView;
