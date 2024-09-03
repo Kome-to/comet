30d14d95384ff03f4fe363baf6222a5d18a1ba32
Duc Anh <anh.chuduc@politetech.com>
2024-09-04 00:10:41 +0700

    server: update graphql setup

--- server/src/interfaces/Workspace.ts
+++ server/src/interfaces/Workspace.ts
@@ -1,3 +1,5 @@
+import { MemberCreation } from './Member';
+
 export interface WorkspaceAttributes {
   id: string;
   name: string;
@@ -7,12 +9,14 @@ export interface WorkspaceAttributes {
 export type WorkspaceCreation = Omit<WorkspaceAttributes, 'id'>;
 
 export interface WorkspaceCreateParams {
+  id?: string;
   name: string;
 
-  members: string[];
+  members: MemberCreation[];
 
   channel: {
     name: string;
+    isPrivate: boolean;
     publicKey: string;
     ePrivateKey: string;
   };
